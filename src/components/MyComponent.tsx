a7ca72e0d6f414eeb2442e9aa9446c0da82486cd
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-04-13 20:11:55 +0300

    Lesson 2 add style

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,10 +1,10 @@
 import {FC} from "react";
-
+import styles from './MyComponent.module.css';
 type MyComponentPropType = {text:string}
 
 const MyComponent: FC<MyComponentPropType> = ({text}) => {
     return (
-        <div className={'target'}>
+        <div className={styles.target}>
             {text}
         </div>
     );
