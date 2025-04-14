4f61c2a6e91e2ee23dc1fa4b5ea708579bae184e
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-04-14 11:46:10 +0300

    Lesson 3 tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
-import MyComponent from "./components/MyComponent.tsx";
+// import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-        <MyComponent text={'Hello Okten 1'}/>
-        <MyComponent text={'Hello Okten 2'}/>
-        <MyComponent text={'Hello Okten 3'}/>
-        {/*{MyComponent({text: 'Hello Okten 2'})}*/}
+        <ul>
+            <li>asd</li>
+            <li>asd</li>
+            <li>asd</li>
+            <li>asd</li>
+        </ul>
 
     </>
   )
