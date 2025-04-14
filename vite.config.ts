014e8466b93b6c54862986e62a14068ad3de520d
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-04-14 17:33:44 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,10 @@
 
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
